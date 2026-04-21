6e41ffc42b2dc70bbfbad0e278b5896cb37158a4
maxonkizino <mkizino83@gmail.com>
2026-04-21 12:22:57 +0300

    first commit

---
+++ src/main/java/com/javainternshippaymentservice/JavaInternshipPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.javainternshippaymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaInternshipPaymentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaInternshipPaymentServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javainternshippaymentservice/JavaInternshipPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javainternshippaymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaInternshipPaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
